8c9b7fd137bf2db5152da3ecf536ab9845c0bcd4
DavidEscobar007 <jescobar573@misena.edu.co>
2019-09-23 20:17:51 -0500

    learning some of shallow rendering from enzyme.....working with a simple todoApp

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Todo } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+//to configure enzyme
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  describe("Todo", () => {
+    it('run completeTodo when Complete is clicked', () => {
+      const completeTodo = jest.fn();
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        isCompleted: true,
+        texto: 'lala'
+      }
+      const wrapper = shallow(
+        <Todo
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          index={index}
+          todo={todo}
+        />);
+      wrapper.find('button').at(0).simulate('click');
+      expect(completeTodo.mock.calls).toEqual([[5]])
+      expect(removeTodo.mock.calls).toEqual([])
+    });
+
+    it('run removeTodo when remove is clicked', () => {
+      const completeTodo = jest.fn();
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        isCompleted: true,
+        texto: 'lala'
+      }
+      const wrapper = shallow(
+        <Todo
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          index={index}
+          todo={todo}
+        />);
+      wrapper.find('button').at(1).simulate('click');
+      expect(removeTodo.mock.calls).toEqual([[5]])
+      expect(completeTodo.mock.calls).toEqual([])
+    });
+
+  })
+
+})
